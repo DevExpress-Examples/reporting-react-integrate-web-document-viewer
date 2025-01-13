7034f76b9d58d98eec9d8fc01c1c23d76381bbad
Polina Tyureva <polina.tyureva@devexpress.com>
2025-01-13 17:45:36 +0400

    add solution

---
+++ ServerApp/Controllers/ReportingControllers.cs
@@ -0,0 +1,10 @@
+﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerApp.Controllers {
+    public class CustomWebDocumentViewerController : WebDocumentViewerController {
+        public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService) : base(controllerService) {
+        }
+    }
+}
